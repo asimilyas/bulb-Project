1e1fe76655be0e5248e4a51f9e5d13afb8a10491
asimilyas <ilyas.aasim@gmail.com>
2020-07-12 15:26:52 +0500

    bulb project files

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function bulbON(){
+    
+    document.getElementById('myImage').src = 'pic_bulbon.gif'
+}
+
+function bulbOFF(){
+    
+    document.getElementById('myImage').src = 'pic_bulboff.gif'
+}
